f19ffc681b8a4dd86afe23df98fb2415511857a9
Chris Haines <chris@lot18.com>
2015-10-12 17:12:58 -0400

    Use requests library.

--- ziptastic/ziptastic.py
+++ ziptastic/ziptastic.py
@@ -1,40 +1,44 @@
 import urllib2
 import json
+import requests
 
 
 class Ziptastic(object):
     """Ziptastic Python Module"""
 
-    #: This is your Ziptastic API Key that you can get from
-    #: https://www.getziptastic.com/dashboard
-    api_key = ''
-
     #: The current endpoint where Ziptastic APIs are served from.
     endpoint = 'zip.getziptastic.com'
+
+    def __init__(self, api_key):
+        """
+        Initialize Ziptastic API
+
+        :param: This is your Ziptastic API Key that you can get from
+            https://www.getziptastic.com/dashboard
+        """
+        self.api_key = api_key
 
     @staticmethod
     def build_url(endpoint, version='v3', preferred_protocol='https'):
         """Build the Ziptastic API url."""
-        return preferred_protocol + '://' + endpoint + '/' + version + '/'
+        return preferred_protocol + '://' + endpoint + '/' + version
 
-    @classmethod
-    def get_from_postal_code(cls, postal_code, country='US'):
+    def get_from_postal_code(self, postal_code, country='US'):
         """Get geo data from postal code and country code"""
         headers = {}
 
         #: If no api_key is set then default to Version 2 of Ziptastic API.
-        if cls.api_key:
+        if self.api_key:
             headers.update({
-                "x-key": cls.api_key
+                "x-key": self.api_key
             })
-            uri = cls.build_url(cls.endpoint)
+            uri = self.build_url(self.endpoint)
         else:
-            uri = cls.build_url(cls.endpoint, version='v2')
+            uri = self.build_url(self.endpoint, version='v2')
 
-        url = uri + str(country) + '/' + str(postal_code)
+        url = "{uri}/{country}/{postal_code}".format(uri=uri, country=country,
+                                                     postal_code=postal_code)
 
-        request = urllib2.Request(url, headers=headers)
-        contents = urllib2.urlopen(request).read()
+        r = requests.get(url, headers=headers)
 
-        #: An object is returned.
-        return json.loads(contents)
+        return r.json()
